8f9d78da2f97a4964154d26120aeb7ac7a54700d
Vipul Sharma <vipulsharma936@gmail.com>
2017-12-03 15:23:12 +0530

    Stemming: Implement Edge N Gram Method (Closes #14)

--- modify_query.py
+++ modify_query.py
@@ -3,9 +3,15 @@ nltk.download("stopwords")
 from nltk.corpus import stopwords
 import string
 
+def stemming(query, n):
+    return zip(*[query[i:] for i in range(n)])
+
 def query(query):
     query = query.strip().lower()
     query = query.translate(string.maketrans("",""), string.punctuation).split(' ')
     stop_words = set(stopwords.words('english'))
     query = [word for word in query if word not in stop_words]
-    return query
+    ngrams_query = []
+    for i in range(len(query)+1):
+        ngrams_query.append(stemming(query, i))
+    return ngrams_query
