29f07a3f95fcbd73d76eae1367aff7f14d1145dc
Vipul Sharma <vipulsharma936@gmail.com>
2017-12-10 06:25:14 -0600

    crawler: Fixed for urls containing the allowed domain

--- extract.py
+++ extract.py
@@ -10,11 +10,12 @@ class axxelerate_spider(CrawlSpider):
     name = 'axxelerate'
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Main_Page']
-    rules = (Rule(LxmlLinkExtractor(allow=()), callback='parse_obj', follow=True),)
+    rules = (Rule(LxmlLinkExtractor(allow=()), callback='parse', follow=True),)
 
-    def parse_obj(self,response):
+    def parse(self,response):
         item = url_item()
         item['url'] = []
-        for link in LxmlLinkExtractor(allow=(),deny = self.allowed_domains).extract_links(response):
-            item['url'].append(link.url)
+        for link in LxmlLinkExtractor(allow=(self.allowed_domains),deny = ()).extract_links(response):
+            if 'en.wikipedia.org' in link.url:
+                item['url'].append(link.url)
         return item
