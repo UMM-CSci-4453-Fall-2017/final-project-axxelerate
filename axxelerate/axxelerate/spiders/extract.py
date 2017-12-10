dc43c499e9c9744d836e9c3c280784a5c50168c3
Vipul Sharma <sharm493@morris.umn.edu>
2017-12-10 14:55:37 -0600

    Added to get the text from the webpages

--- axxelerate/axxelerate/spiders/extract.py
+++ axxelerate/axxelerate/spiders/extract.py
@@ -4,18 +4,20 @@ from scrapy.item import Item, Field
 
 class url_item(Item):
     url= Field()
-
+    keywords = Field()
 
 class axxelerate_spider(CrawlSpider):
     name = 'axxelerate'
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Main_Page']
-    rules = (Rule(LxmlLinkExtractor(allow=()), callback='parse_obj', follow=True),)
+    rules = (Rule(LxmlLinkExtractor(allow=(allowed_domains)), callback='parse_obj', follow=True),)
 
     def parse_obj(self,response):
         item = url_item()
-        item['url'] = []
-        for link in LxmlLinkExtractor(allow=(self.allowed_domains),deny = ()).extract_links(response):
-            if 'en.wikipedia.org' in link.url:
-                item['url'].append(link.url)
+        item['url'] = response.url
+        item['keywords'] = []
+        tags = ["h1", "h2", "h3", "h4", "h5", "h6", "title", "article", "div",
+                "blockquote", "td", "li", "a", "p", "span", "strong", "bold"]
+        for tag in tags:
+            item['keywords'].append(response.xpath("//%s/text()" % (tag)).extract())
         return item
