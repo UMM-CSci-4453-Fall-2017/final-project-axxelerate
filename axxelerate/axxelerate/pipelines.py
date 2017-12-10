8a6c5ab028cf3718816ddb9362b62b95e1f2904d
Laverne Schrock <schr1230@morris.umn.edu>
2017-12-10 13:40:02 -0600

    Use 4 spaces for indent in pipelines.py

--- axxelerate/axxelerate/pipelines.py
+++ axxelerate/axxelerate/pipelines.py
@@ -11,16 +11,16 @@ import credentials
 
 class AxxeleratePipeline(object):
     connection = pymysql.connect(host = credentials.host,
-    						user = credentials.user,
-    						password = credentials.password,
-    						cursorclass = pymysql.cursors.DictCursor)
+                                 user = credentials.user,
+                                 password = credentials.password,
+                                 cursorclass = pymysql.cursors.DictCursor)
 
     def process_item(self, item, spider):
-    	try:
-    		with self.connection.cursor() as cursor:
-    			sql = "INSERT INTO `urlinfo` (`url`) VALUES (%s)"
-    			cursor.execute(sql, (item['url']))
-    		self.connection.commit()
-    	except: 
-    		pass
-    	return item
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "INSERT INTO `urlinfo` (`url`) VALUES (%s)"
+                cursor.execute(sql, (item['url']))
+            self.connection.commit()
+        except: 
+            pass
+        return item
