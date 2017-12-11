1b738fe55e138747e9701f20c762980d7beb38a1
Vipul Sharma <sharm493@morris.umn.edu>
2017-12-10 20:13:22 -0600

    Update keyword insertion to improve speed
    Instead of doing one INSERT per keyword, we now insert all the
    keywords at once, which is SIGNIFICANTLY faster.

--- axxelerate/axxelerate/pipelines.py
+++ axxelerate/axxelerate/pipelines.py
@@ -19,9 +19,25 @@ class AxxeleratePipeline(object):
     def process_item(self, item, spider):
         try:
             with connection.cursor() as cursor:
-                sql = "INSERT INTO `pages` (`url`, `title`) VALUES (%s, %s)"
-                cursor.execute(sql, (item['url'], item['title']))
+                sql_url_title = "INSERT INTO `pages` (`url`, `title`) VALUES (%s, %s)"
+                cursor.execute(sql_url_title, (item['url'], item['title']))
+
+                pageID = cursor.lastrowid
+
+                placeHolders = []
+                valuesToInsert = []
+
+                for word in item['keywords']:
+                    valuesToInsert.append(word)
+                    valuesToInsert.append(pageID)
+                    placeHolders.append("(%s,%s)")
+
+                if len(placeHolders) > 0:
+                    sql_keywords = "INSERT INTO `keywords` (`word`, `pageID`) VALUES " + (",".join(placeHolders))
+                    cursor.execute(sql_keywords, valuesToInsert)
+
             connection.commit()
+
         except:
             pass
         return item
