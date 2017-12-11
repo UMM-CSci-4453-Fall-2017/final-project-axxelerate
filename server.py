3a54a1e85e32d82597324691b0a894fdf0c5fc72
Laverne Schrock <schr1230@morris.umn.edu>
2017-12-10 23:54:31 -0600

    Connect frontend to DB
    There a huge number of problems here
    
    1. There is no pagination.
    2. It is incredibly slow (presumably adding our indexes will improve
    this)
    3. Currently expects only one keyword.
    4. Irrelevant/poorly ranked results (adding support for multiple words
    in the query will help a little bit).

--- server.py
+++ server.py
@@ -2,7 +2,15 @@ from flask import Flask, request, Response, abort
 from flask_cors import cross_origin
 import json
 import example_data
+import pymysql
+import credentials
 app = Flask(__name__)
+
+connection = pymysql.connect(host = credentials.host,
+                             user = credentials.user,
+                             password = credentials.password,
+                             db = credentials.db,
+                             cursorclass=pymysql.cursors.DictCursor)
 
 @app.route("/")
 def hello():
@@ -23,16 +31,38 @@ def getResults():
     #     page = page
 
     # TODO: At some point we need to break down the query into keywords
+    #       For now, we'll assume that it is a single keyword
 
-    if (queryString == "result0"):
-        result = example_data.result0
-    elif (queryString == 'result1' and page != "2"):
-        result = example_data.result1_1
-    elif (queryString == 'result1' and page == "2"):
-        result = example_data.result1_2
-    else:
+    # if (queryString == "result0"):
+    #     result = example_data.result0
+    # elif (queryString == 'result1' and page != "2"):
+    #     result = example_data.result1_1
+    # elif (queryString == 'result1' and page == "2"):
+    #     result = example_data.result1_2
+    # else:
+    #     result = {
+    #         "results" : []
+    #     }
+
+    result = None
+
+    with connection.cursor() as cursor:
+        sql_request = "SELECT url, title FROM keywords LEFT JOIN pages ON pageID = ID WHERE word = %s";
+        cursor.execute(sql_request, queryString)
+        raw_results = cursor.fetchall()
+
         result = {
+            "nextPage" : "",
+            "prevPage" : "",
             "results" : []
         }
+
+        for page in raw_results:
+            p = {
+                "snippet" : "",
+                "link" : page["url"].decode("latin1"),
+                "title" : page["title"].decode("latin1")
+            }
+            result["results"].append(p)
 
     return Response(json.dumps(result), mimetype="application/json")
