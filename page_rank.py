c2c8deea08e9ee2f4eb46f76139d16aa8e268a41
Laverne Schrock <schr1230@morris.umn.edu>
2017-12-12 03:05:04 -0600

    Read and write data for pagerank to db
    This is too slow (computationally, not DB IO).

--- page_rank.py
+++ page_rank.py
@@ -1,40 +1,71 @@
 #! /usr/bin/env python
 
-import time
+import sys
+import pymysql.cursors
+import credentials
+import pprint
 
 damping = 0.85
 
 def main():
 
-    # assume we have a list of nodes and weights
-    nodes_and_ranks = {
-        "Home" : 1,
-        "About" : 1,
-        "Product" : 1,
-        "More" : 1
-    }
+    pp = pprint.PrettyPrinter(indent=4)
 
-    # assume we have the linking table
-    links = {
-        "Home" : ["About", "Product", "More"],
-        "About" : ["Home"],
-        "Product" : ["Home"],
-        "More" : ["Home"]
-    }
+    connection = pymysql.connect(host = credentials.host,
+                                 user = credentials.user,
+                                 password = credentials.password,
+                                 db = credentials.db,
+                                 cursorclass = pymysql.cursors.DictCursor)
 
-    for i in range(1,40):
-        export_strengths = {n: export_strength(nodes_and_ranks, links, n) for n in links}
-        new_ranks = {n: compute_new_pr(export_strengths, links, n) for n in links}
-        differences = []
-        for node in new_ranks:
-            diff =  abs(new_ranks[node] - nodes_and_ranks[node])
-            differences.append(diff)
-        print(differences)
-        print(new_ranks)
-        avg_diff = sum(differences)/len(differences)
-        print avg_diff
-        print
-        nodes_and_ranks = new_ranks
+    nodes_and_ranks = None
+    links = None
+
+
+    with connection.cursor() as cursor:
+
+        sql_hash_list = "SELECT `fromHash`, `toHash` FROM `linksTo`"
+        sql_page_list = "SELECT `urlHash` FROM `pages`"
+
+        cursor.execute(sql_page_list)
+        original_from_db = cursor.fetchall()
+        print("fetched first chunk")
+
+        links = { row['urlHash'] : [] for row in original_from_db}
+
+        cursor.execute(sql_hash_list)
+        all_hashes = cursor.fetchall()
+
+        for row in all_hashes:
+            src = row['fromHash']
+            dst = row['toHash']
+            links[src].append(dst)
+
+        print("fetched last chunk")
+
+        nodes_and_ranks = {h: 0.15 for h in links}
+
+        iteration = 0
+        avg_diff = float("inf")
+
+        while avg_diff > 0.00001:
+            export_strengths = {n: export_strength(nodes_and_ranks, links, n) for n in links}
+            new_ranks = {n: compute_new_pr(export_strengths, links, n) for n in links}
+            differences = []
+            for node in new_ranks:
+                diff =  abs(new_ranks[node] - nodes_and_ranks[node])
+                differences.append(diff)
+            avg_diff = sum(differences)/float(len(differences))
+            nodes_and_ranks = new_ranks
+            print("%s %s" % (iteration, avg_diff))
+            iteration += 1
+
+        print("start writing data")
+        sql_update_result = "UPDATE `pages` SET `pagerank` = %s WHERE `urlHash` = %s"
+        for h,rank in nodes_and_ranks.iteritems():
+            cursor.execute(sql_update_result, (rank,h))
+        print("done writing data")
+
+    connection.commit()
 
 
 def export_strength(prev_ranks, links, node):
